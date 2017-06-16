0bb203145ac5aa19f722a6c8658f20cbff8495fe
Steven Fackler <sfackler@gmail.com>
2017-06-15 21:42:21 -0700

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! Tokio wrappers which apply timeouts to IO operations.
+//!
+//! These timeouts are analagous to the read and write timeouts on traditional
+//! blocking sockets. A timeout countdown is initiated when a read/write
+//! operation returns `WouldBlock`. If a read/write does not return successfully
+//! the before the countdown expires, `TimedOut` is returned.
 extern crate futures;
 extern crate tokio_core;
 extern crate tokio_io;
@@ -13,6 +19,7 @@ use tokio_io::{AsyncRead, AsyncWrite};
 use tokio_proto::BindServer;
 use tokio_service::Service;
 
+/// An `AsyncRead`er which applies a timeout to read operations.
 pub struct TimeoutReader<R> {
     reader: R,
     handle: Handle,
@@ -23,6 +30,9 @@ pub struct TimeoutReader<R> {
 impl<R> TimeoutReader<R>
     where R: AsyncRead
 {
+    /// Returns a new `TimeoutReader` wrapping the specified reader.
+    ///
+    /// There is initially no timeout.
     pub fn new(reader: R, handle: &Handle) -> TimeoutReader<R> {
         TimeoutReader {
             reader,
@@ -32,23 +42,30 @@ impl<R> TimeoutReader<R>
         }
     }
 
+    /// Returns the current read timeout.
     pub fn timeout(&self) -> Option<Duration> {
         self.timeout
     }
 
+    /// Sets the read timeout.
+    ///
+    /// This will reset any pending timeout.
     pub fn set_timeout(&mut self, timeout: Option<Duration>) {
         self.timeout = timeout;
         self.cur = None;
     }
 
+    /// Returns a shared reference to the inner reader.    
     pub fn get_ref(&self) -> &R {
         &self.reader
     }
 
+    /// Returns a mutable reference to the inner reader.
     pub fn get_mut(&mut self) -> &mut R {
         &mut self.reader
     }
 
+    /// Consumes the `TimeoutReader`, returning the inner reader.
     pub fn into_inner(self) -> R {
         self.reader
     }
@@ -111,6 +128,7 @@ impl<R> AsyncWrite for TimeoutReader<R>
     }
 }
 
+/// An `AsyncWrite`er which applies a timeout to write operations.
 pub struct TimeoutWriter<W> {
     writer: W,
     handle: Handle,
@@ -121,6 +139,9 @@ pub struct TimeoutWriter<W> {
 impl<W> TimeoutWriter<W>
     where W: AsyncWrite
 {
+    /// Returns a new `TimeoutReader` wrapping the specified reader.
+    ///
+    /// There is initially no timeout.
     pub fn new(writer: W, handle: &Handle) -> TimeoutWriter<W> {
         TimeoutWriter {
             writer,
@@ -130,24 +151,31 @@ impl<W> TimeoutWriter<W>
         }
     }
 
+    /// Returns the current write timeout.
     pub fn timeout(&self) -> Option<Duration> {
         self.timeout
     }
 
+    /// Sets the write timeout.
+    ///
+    /// This will reset any pending timeout.
     pub fn set_timeout(&mut self, timeout: Option<Duration>) {
         self.timeout = timeout;
         self.cur = None;
     }
 
-    pub fn get_ref(&self) -> &W {
+    /// Returns a shared reference to the inner writer.    
+    pub fn get_ref(&self) -> &R {
         &self.writer
     }
 
-    pub fn get_mut(&mut self) -> &mut W {
+    /// Returns a mutable reference to the inner writer.
+    pub fn get_mut(&mut self) -> &mut R {
         &mut self.writer
     }
 
-    pub fn into_inner(self) -> W {
+    /// Consumes the `TimeoutWriter`, returning the inner writer.
+    pub fn into_inner(self) -> R {
         self.writer
     }
 }
@@ -211,42 +239,57 @@ impl<W> AsyncRead for TimeoutWriter<W>
     }
 }
 
+/// A stream which applies read and write timeouts to an inner stream.
 // TODO this stores two copies of the Handle which is maybe not great?
 pub struct TimeoutStream<S>(TimeoutReader<TimeoutWriter<S>>);
 
 impl<S> TimeoutStream<S>
     where S: AsyncRead + AsyncWrite
 {
+    /// Returns a new `TimeoutStream` wrapping the specified stream.
+    ///
+    /// There is initially no read or write timeout.
     pub fn new(stream: S, handle: &Handle) -> TimeoutStream<S> {
         let writer = TimeoutWriter::new(stream, handle);
         let reader = TimeoutReader::new(writer, handle);
         TimeoutStream(reader)
     }
 
+    /// Returns the current read timeout.
     pub fn read_timeout(&self) -> Option<Duration> {
         self.0.timeout()
     }
 
+    /// Sets the read timeout.
+    ///
+    /// This will reset any pending read timeout.
     pub fn set_read_timeout(&mut self, timeout: Option<Duration>) {
         self.0.set_timeout(timeout)
     }
 
+    /// Returns the current write timeout.
     pub fn write_timeout(&self) -> Option<Duration> {
         self.0.get_ref().timeout()
     }
 
+    /// Sets the write timeout.
+    ///
+    /// This will reset any pending write timeout.    
     pub fn set_write_timeout(&mut self, timeout: Option<Duration>) {
         self.0.get_mut().set_timeout(timeout)
     }
 
+    /// Returns a shared reference to the inner stream.
     pub fn get_ref(&self) -> &S {
         self.0.get_ref().get_ref()
     }
 
+    /// Returns a mutable reference to the inner stream.
     pub fn get_mut(&mut self) -> &mut S {
         self.0.get_mut().get_mut()
     }
 
+    /// Consumes the stream, returning the inner stream.
     pub fn into_inner(self) -> S {
         self.0.into_inner().into_inner()
     }
@@ -288,6 +331,8 @@ impl<S> AsyncWrite for TimeoutStream<S>
     }
 }
 
+/// A `BindServer` which wraps streams in a `TimeoutStream` before delegating
+/// to another `BindServer`.
 pub struct TimeoutServerBinder<Kind, T, P>
     where P: BindServer<Kind, TimeoutStream<T>>,
           T: 'static
@@ -303,6 +348,9 @@ impl<Kind, T, P> TimeoutServerBinder<Kind, T, P>
           T: AsyncRead + AsyncWrite + 'static,
           Kind: 'static
 {
+    /// Returns a new `TimeoutServerBinder` wrapping the specified binder.
+    ///
+    /// There is initially no read or write timeout.
     pub fn new(proto: P) -> TimeoutServerBinder<Kind, T, P> {
         TimeoutServerBinder {
             proto,
@@ -312,18 +360,22 @@ impl<Kind, T, P> TimeoutServerBinder<Kind, T, P>
         }
     }
 
+    /// Returns the current read timeout.
     pub fn read_timeout(&self) -> Option<Duration> {
         self.read_timeout
     }
 
+    /// Sets the read timeout.
     pub fn set_read_timeout(&mut self, timeout: Option<Duration>) {
         self.read_timeout = timeout;
     }
 
+    /// Returns the current write timeout.
     pub fn write_timeout(&self) -> Option<Duration> {
         self.write_timeout
     }
 
+    /// Sets the write timeout.
     pub fn set_write_timeout(&mut self, timeout: Option<Duration>) {
         self.write_timeout = timeout;
     }
